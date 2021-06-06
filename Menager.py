b47fd5639847afb653526c9592d008fcbe3a4059
Denox <69839857+DevGuy08@users.noreply.github.com>
2021-06-06 19:52:25 +0200

    Update Menager.py

--- Menager.py
+++ Menager.py
@@ -1,57 +1,58 @@
 import os
 directory = os.getcwd()
-print('ci torviamo in', directory)
-inp = input()
-if (inp == '/makedir'):
-    print('come vuoi chiamare la cartella?')
-    r = input()
-    str(r)
-    try:
-        os.mkdir(r)
-        print('cartella creata!')
-    except:
-        print('si è verificato un errore')
-elif (inp == '/delete'):
-    print('che cosa vuoi eliminare?')
-    r = input()
-    str(r)
-    try:
-        os.remove(r)
-        print('File eliminato con successo!')
-    except:
-        print('File non trovato!')
-elif (inp == '/rename'):
-    r = input('Quale file vuoi rinominare?\n')
-    str(r)
-    ri = input('e con quale nome lo vuoi cambiare?\n')
-    str(ri)
-    try:
-        os.rename(r, ri)
-        print('File rinominato con successo!')
-    except:
-        print('Si è verificato un errore.... Riprova!')
+print('''we're currently  in''', directory)
+while True:
+    inp = input()
+    if (inp == '/makedir'):
+        print('how do you wanna name the folder?\n)
+        r = input()
+        str(r)
+        try:
+            os.mkdir(r)
+            print('folder created!')
+        except:
+            print('an error occurred')
+    elif (inp == '/delete'):
+        print('which file do you wanna delete?\n')
+        r = input()
+        str(r)
+        try:
+            os.remove(r)
+            print('file eliminated successfully!')
+        except:
+            print('impossible find the file!')
+    elif (inp == '/rename'):
+        r = input('which file do you wanna delete?\n')
+        str(r)
+        ri = input('and what name do you want to change it to?\n')
+        str(ri)
+        try:
+            os.rename(r, ri)
+            print('file renamed successfully!')
+        except:
+            print('an error occurred')
 
-elif (inp == '/removedir'):
-    r = input('Quale cartella vuoi eliminare?\n')
-    str(r)
-    try:
-        os.rmdir(r)
-        print('File rinominato con successo!')
-    except:
-        print('Cartella non trovata')
+    elif (inp == '/removedir'):
+        r = input('which file do you wanna delete?\n')
+        str(r)
+        try:
+            os.rmdir(r)
+            print('folder eliminated successfully!')
+        except:
+            print('impossible find the folder')
 
-elif (inp == '/where'):
-   print(directory)
+    elif (inp == '/where'):
+       print(directory)
 
-elif(inp == '/moveto'):
-    r = input('Dove vuoi andare?')
-    try:
-        os.chdir(r)
-    except:
-        print('directory non trovata!')
+    elif(inp == '/moveto'):
+        r = input('Where do you want to go?\n')
+        try:
+            os.chdir(r)
+        except:
+            print('directory inexistent')
 
-elif (inp == '/what'):
-    print(os.listdir())
+    elif (inp == '/what'):
+        print(os.listdir())
 
-else:
-    print('comando sconosciuto')
+    else:
+        print('unknow command')
